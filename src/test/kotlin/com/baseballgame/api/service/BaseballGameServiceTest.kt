9dc031cb576fe4505552eece6358d1dee2b833ed
Ian <ian@myfair.co>
2025-06-03 18:08:14 +0900

    feat: test 코드 temp

---
+++ src/test/kotlin/com/baseballgame/api/service/BaseballGameServiceTest.kt
@@ -0,0 +1,71 @@
+package com.baseballgame.api.service
+
+import com.baseballgame.api.dto.HistoryDto
+import com.baseballgame.api.dto.PlayerDto
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.Rollback
+import org.springframework.transaction.annotation.Transactional
+
+
+
+@SpringBootTest
+@Transactional
+class BaseballGameServiceTest(
+    @Autowired private val baseballGameService: BaseballGameService
+) {
+    @Test
+//    @Rollback(false)
+    fun createGame() {
+        val gameName = "테스트 게임"
+        val createdGame = baseballGameService.createGame(gameName)
+        assertNotNull(createdGame.id)
+        assertEquals(gameName, createdGame.name)
+        assertFalse(createdGame.isEnd)
+
+        val foundGame = createdGame.id?.let { baseballGameService.findGame(it) }
+        assertEquals(createdGame.id, foundGame?.id)
+    }
+
+    @Test
+    fun deleteGame() {
+        val gameName = "삭제 테스트 게임"
+        val createdGame = baseballGameService.createGame(gameName)
+        assertNotNull(createdGame.id)
+
+        baseballGameService.deleteGame(createdGame.id!!)
+
+        assertThrows(NoSuchElementException::class.java) {
+            baseballGameService.findGame(createdGame.id!!)
+        }
+    }
+
+    @Test
+    fun updateGame() {
+        val gameName = "업데이트 테스트 게임"
+        val createdGame = baseballGameService.createGame(gameName)
+        assertNotNull(createdGame.id)
+
+        val updatedPlayers = listOf(
+            PlayerDto(
+                isWinner = true,
+                history = listOf(
+                    HistoryDto(input = "123", strike = 2, ball = 1)
+                )
+            )
+        )
+
+        baseballGameService.updateGame(createdGame.id!!, true, updatedPlayers)
+
+        val updatedGame = baseballGameService.findGame(createdGame.id!!)
+        assertTrue(updatedGame.isEnd)
+        assertEquals(1, updatedGame.players.size)
+        assertEquals(true, updatedGame.players.first().isWinner)
+        assertEquals("123", updatedGame.players.first().history.first().input)
+        assertEquals(2, updatedGame.players.first().history.first().strike)
+        assertEquals(1, updatedGame.players.first().history.first().ball)
+    }
+
+}
